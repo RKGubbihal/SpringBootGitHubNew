2e1fbc8cbd847c39054250db00f345f6082214f2
RK <AWSDevOpsProfessional2023@gmail.com>
2025-05-26 01:28:37 -0700

    3rd Commit in the IntelliJ IDE

--- src/main/java/com/stechg/SpringBootGit/Controller.java
+++ src/main/java/com/stechg/SpringBootGit/Controller.java
@@ -1,7 +1,6 @@
 package com.stechg.SpringBootGit;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller
@@ -11,4 +10,33 @@ public class Controller
     {
         return "Message_'SpringBootGit' Spring Boot Application_IntelliJ IDE.....";
     }
+
+    @GetMapping("/gethello")
+    public String getHello()
+    {
+        System.out.println("Displaying  Message from the 'GetMapping' Method ");
+        return "<h1> 'GetMapping' Method: Message </h1>";
+    }
+
+    @PostMapping("/posthello")
+    public String postHello()
+    {
+        System.out.println("Posting Message from the 'PostMapping' Method ");
+        return "<h1> 'PostMapping' Method: Message </h1>";
+    }
+
+    @PutMapping("/puthello")
+    public String putHello()
+    {
+        System.out.println("Putting Message from the 'PutMapping' Method ");
+        return "<h1> 'PutMapping' Method: Message </h1>";
+    }
+
+    @DeleteMapping("/deletehello")
+    public String deleteHello()
+    {
+        System.out.println("Deleting Message from the 'DeleteMapping' Method ");
+        return "<h1> 'DeleteMapping' Method: Message </h1>";
+    }
+
 }
